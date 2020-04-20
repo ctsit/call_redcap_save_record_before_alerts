e565059ae167082030396f08b7b688691215ef70
Philip Chase <pbc@ufl.edu>
2020-04-20 17:19:41 -0400

    Add patch and a deploy script

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# deployment script for the change_from_address patch for the ReminderUserAccessDashboardEmail cron job
+# Clone the repo that has this file via
+#
+#   git clone git@github.com:ctsit/change_from_address_in_dashboard_reminder.git
+#
+# Call this script with two parameters:
+#
+#   REDCAP_ROOT - the top level redcap directory.  typically this directory contains cron.php
+#   REDCAP_VERSION - the version of the code being patched/deployed.
+#
+
+set -e
+
+export REDCAP_ROOT=$1
+export REDCAP_VERSION=$2
+
+# determine the directory where this script resides
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $REDCAP_ROOT/redcap_v$REDCAP_VERSION/Classes/
+patch -p1 < $DIR/patch.patch
